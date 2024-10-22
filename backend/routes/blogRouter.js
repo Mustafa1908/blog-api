86d06e53a282b0850227f16168bb7c65e700fb6d
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-10-22 20:47:20 +0200

    Change routes names

--- backend/routes/blogRouter.js
+++ backend/routes/blogRouter.js
@@ -3,6 +3,7 @@ const blogController = require("../controllers/blogController");
 
 const router = express.Router();
 
-router.post("/blog/create_post", blogController.createPost);
+router.post("/blog/post", blogController.createPost);
+router.post("/blog/post/comment", blogController.createComment);
 
 module.exports = router;
