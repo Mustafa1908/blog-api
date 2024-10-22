016fa2e661798d245d9bc413e2376d51444af7b9
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-10-22 20:46:33 +0200

    Add createComment controller

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -32,6 +32,35 @@ const createPost = async (req, res) => {
   }
 };
 
+const createComment = async (req, res) => {
+  const { comment } = req.body;
+
+  if (!req.session || !req.session.userId) {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const userComment = await prisma.comment.create({
+      data: {
+        comment,
+        authorId: req.session.userId,
+      },
+    });
+
+    res.status(201).json({
+      message: "Post created successfully",
+      post: {
+        comment: userComment.comment,
+        authorId: userComment.authorId,
+      },
+    });
+  } catch (error) {
+    console.error("Error creating post:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 module.exports = {
   createPost,
+  createComment,
 };
