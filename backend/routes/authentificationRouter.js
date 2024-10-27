e043c2253b327bddccf39e45b17d8794ac231d23
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-10-27 17:30:56 +0100

    Change routes paths names, add alluser route

--- backend/routes/authentificationRouter.js
+++ backend/routes/authentificationRouter.js
@@ -3,7 +3,10 @@ const authentificationController = require("../controllers/authentificationContr
 
 const router = express.Router();
 
+router.get("/alluser", authentificationController.getAllUsers);
 router.post("/user", authentificationController.registerUser);
-router.post("/log_in", authentificationController.loginUser);
+router.post("/login", authentificationController.loginUser);
+router.post("/logout", authentificationController.logoutUser);
+router.get("/check/session", authentificationController.checkSession);
 
 module.exports = router;
