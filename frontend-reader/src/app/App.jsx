b2b18d1ec51a838b21d04330347ae11c60802c29
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-11-01 16:48:25 +0100

    Import router,  router functions, use router

--- frontend-reader/src/app/App.jsx
+++ frontend-reader/src/app/App.jsx
@@ -1,42 +1,14 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import routes from "./router";
+
+const router = createBrowserRouter(createRoutesFromElements(routes));
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await axios.get("http://localhost:8000/users"); // Your API URL
-        setUsers(response.data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchUsers();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
-
-  console.log(users);
-  return (
-    <div>
-      <h1>Users List</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.username} - {user.email}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
