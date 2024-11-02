20ac6435ea8c5be60fecc4af37fe4a69b8dbbe13
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-11-02 16:40:40 +0100

    Add userInformations, all posts

--- frontend-reader/src/pages/Blog.jsx
+++ frontend-reader/src/pages/Blog.jsx
@@ -1,16 +1,39 @@
 import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
 
 const Blog = () => {
   const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const getUserInfo = () => {
       const token = localStorage.getItem("token");
-      console.log(JSON.parse(window.atob(token.split(".")[1])));
+      if (token) {
+        try {
+          const decoded = JSON.parse(window.atob(token.split(".")[1]));
+          setUser(decoded);
+        } catch (error) {
+          console.error("Token decoding error:", error);
+        }
+      }
+    };
+
+    const getAllPosts = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/post");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        setError("Failed to fetch posts");
+      }
     };
 
     getUserInfo();
+    getAllPosts();
   }, []);
 
   const handleLogout = () => {
@@ -23,14 +46,23 @@ const Blog = () => {
       <h1>Blog Component</h1>
       {user ? (
         <div>
-          <h2>Welcome, {user.name}!</h2>
-          <p>Email: {user.email}</p>
+          <p>Id: {user.userId}</p>
+          <h2>Welcome, {user.username}!</h2>
           <button onClick={handleLogout}>Logout</button>
         </div>
       ) : (
         <div>Please log in to see your information.</div>
       )}
-      <Outlet />
+      <h1>Blog Posts</h1>
+      {error && <p>{error}</p>}
+      <div>
+        {posts.map((post) => (
+          <a href={`/blog/${post.id}`} key={post.id}>
+            <span>{post.postTitle}</span>
+            <p> {post.postText}</p>
+          </a> // Adjust based on your post structure
+        ))}
+      </div>
     </div>
   );
 };
