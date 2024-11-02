7befeeec8443c10b7f39f040a0feed108a8d192c
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-11-02 16:39:59 +0100

    Add navbar, post, change index element

--- frontend-reader/src/app/router.jsx
+++ frontend-reader/src/app/router.jsx
@@ -1,12 +1,16 @@
+import Navbar from "../components/Navbar.jsx";
 import Blog from "../pages/Blog.jsx";
 import Register from "../pages/Register.jsx";
 import Login from "../pages/Login.jsx";
+import Post from "../pages/Post.jsx";
 import { Route } from "react-router-dom";
 
 const routes = (
-  <Route path="/" element={<Blog />}>
+  <Route path="/" element={<Navbar />}>
+    <Route path="blog" element={<Blog />} />
+    <Route path="blog/:postId" element={<Post />} />
     <Route path="register" element={<Register />} />
-    <Route path="login" element={<Login />}></Route>
+    <Route path="login" element={<Login />} />
   </Route>
 );
 
