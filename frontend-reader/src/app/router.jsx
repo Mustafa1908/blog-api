f3b250bb99c0d950332b5d479b26c435aa7ad9aa
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-11-10 17:30:36 +0100

    Change post path, add posts route, change import paths names

--- frontend-reader/src/app/router.jsx
+++ frontend-reader/src/app/router.jsx
@@ -1,14 +1,16 @@
-import Navbar from "../components/Navbar.jsx";
-import Blog from "../pages/Blog.jsx";
-import Register from "../pages/Register.jsx";
-import Login from "../pages/Login.jsx";
-import Post from "../pages/Post.jsx";
+import Navbar from "../components/Navbar/Navbar.jsx";
+import Blog from "../pages/Blog/Blog.jsx";
+import Register from "../pages/Register/Register.jsx";
+import Login from "../pages/Login/Login.jsx";
+import Post from "../pages/Post/Post.jsx";
+import Posts from "../pages/Posts/Posts.jsx";
 import { Route } from "react-router-dom";
 
 const routes = (
   <Route path="/" element={<Navbar />}>
     <Route path="blog" element={<Blog />} />
-    <Route path="blog/:postId" element={<Post />} />
+    <Route path="blog/post/:postId" element={<Post />} />
+    <Route path="post" element={<Posts />}></Route>
     <Route path="register" element={<Register />} />
     <Route path="login" element={<Login />} />
   </Route>
