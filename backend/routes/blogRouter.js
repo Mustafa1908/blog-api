5f1684975c251e415bd73672628d447ca7aab41e
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-11-13 17:05:12 +0100

    Remove comment, replace key by env secret_key

--- backend/routes/blogRouter.js
+++ backend/routes/blogRouter.js
@@ -2,25 +2,28 @@ const express = require("express");
 const blogController = require("../controllers/blogController");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
+const dotenv = require("dotenv");
 
+dotenv.config();
 const verifyToken = (req, res, next) => {
   const token = req.headers["authorization"]?.split(" ")[1];
   if (!token) {
-    // Return immediately if no token is provided
     return res.status(403).json({ message: "No token provided" });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET || "secretkey", (err, authData) => {
-    if (err) {
-      console.log("Token verification error:", err);
-      // Return immediately if the token is invalid
-      return res.status(403).json({ message: "Unauthorized" });
-    }
+  jwt.verify(
+    token,
+    process.env.JWT_SECRET || process.env.SECRET_KEY,
+    (err, authData) => {
+      if (err) {
+        console.log("Token verification error:", err);
+        return res.status(403).json({ message: "Unauthorized" });
+      }
 
-    // If the token is valid, attach the decoded data to the request and call next
-    req.authData = authData;
-    next(); // Proceed to the next middleware or route handler
-  });
+      req.authData = authData;
+      next();
+    }
+  );
 };
 
 router.get("/post", blogController.getAllPosts);
@@ -32,7 +35,6 @@ router.patch(
   verifyToken,
   blogController.changePublishedStatue
 );
-router.put("/blog/:post/edit", verifyToken, blogController.editPost);
 router.delete("/blog/post/:post", verifyToken, blogController.deletePost);
 router.post(
   "/blog/:userId/:postId/comment",
