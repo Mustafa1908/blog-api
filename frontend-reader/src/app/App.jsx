acd248af4176d5aaadca5a78ad8da8c980793dab
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-11-13 16:02:49 +0100

    Import createContext, useState, export UserTokenContext, add Provider in return with token useState

--- frontend-reader/src/app/App.jsx
+++ frontend-reader/src/app/App.jsx
@@ -3,12 +3,21 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
+import { createContext, useState } from "react";
 import routes from "./router";
 
 const router = createBrowserRouter(createRoutesFromElements(routes));
 
+export const UserTokenContext = createContext();
+
 const App = () => {
-  return <RouterProvider router={router} />;
+  const [token, setToken] = useState(null);
+
+  return (
+    <UserTokenContext.Provider value={{ token, setToken }}>
+      <RouterProvider router={router} />
+    </UserTokenContext.Provider>
+  );
 };
 
 export default App;
