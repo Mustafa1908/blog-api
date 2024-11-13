b5c6f1db08a0d26697756fa3e6616106cbf0d5f9
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-11-13 17:36:05 +0100

    Add verifyToken util function

---
+++ backend/utils/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const verifyToken = (req, res, next) => {
+  console.log("hey");
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) {
+    return res.status(403).json({ message: "No token provided" });
+  }
+
+  jwt.verify(
+    token,
+    process.env.JWT_SECRET || process.env.SECRET_KEY,
+    (err, authData) => {
+      if (err) {
+        console.log("Token verification error:", err);
+        return res.status(403).json({ message: "Unauthorized" });
+      }
+
+      req.authData = authData;
+      next();
+    }
+  );
+};
+
+module.exports = { verifyToken };
