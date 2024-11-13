e95e70bafcc4e5d02344e80357ca777f5dba44d4
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-11-13 15:56:28 +0100

    Import useContext, UserTokenContext, token useContext variable, setToken to user token when login

--- frontend-reader/src/pages/Login/Login.jsx
+++ frontend-reader/src/pages/Login/Login.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserTokenContext } from "../../app/App";
 
 const Login = () => {
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
+  const { token, setToken } = useContext(UserTokenContext);
   const navigate = useNavigate();
 
   const handleLoginChange = (e) => {
@@ -30,6 +32,7 @@ const Login = () => {
       if (response.ok) {
         const userLogin = await response.json();
         localStorage.setItem("token", userLogin.token);
+        setToken(userLogin.token);
         navigate("/blog");
       } else {
         const errorData = await response.json();
