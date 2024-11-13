2fbb56b1324a2332c03be27542c1aefc81f5b373
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-11-13 15:58:34 +0100

    Chagne usestate name, data variable name, render only posts with published equal to true

--- frontend-reader/src/pages/Blog/Blog.jsx
+++ frontend-reader/src/pages/Blog/Blog.jsx
@@ -1,65 +1,39 @@
-import { useEffect, useState, useContext } from "react";
-import { UserContext } from "../../app/App";
+import { useEffect, useState } from "react";
 
 const Blog = () => {
-  const [user, setUser] = useState(null);
   const [posts, setPosts] = useState([]);
-  const { userRole, setUserRole } = useContext(UserContext);
+
+  const getAllPosts = async () => {
+    try {
+      const allPosts = await fetch("http://localhost:8000/post");
+      if (!allPosts.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const posts = await allPosts.json();
+      setPosts(posts);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
 
   useEffect(() => {
-    const getUserInfo = () => {
-      const token = localStorage.getItem("token");
-      if (token) {
-        try {
-          const decoded = JSON.parse(window.atob(token.split(".")[1]));
-          setUser(decoded);
-        } catch (error) {
-          console.error("Token decoding error:", error);
-        }
-      }
-    };
-
-    const getAllPosts = async () => {
-      try {
-        const allPosts = await fetch("http://localhost:8000/post");
-        if (!allPosts.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await allPosts.json();
-        setPosts(data);
-      } catch (error) {
-        console.error("Error fetching posts:", error);
-      }
-    };
-
-    getUserInfo();
     getAllPosts();
   }, []);
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    setUser(null);
-  };
 
   return (
     <div>
       <h1>Blog Component</h1>
-      {user ? (
-        <div>
-          <p>Id: {user.userId}</p>
-          <h2>Welcome, {user.username}!</h2>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <div>Please log in to see your information.</div>
-      )}
       <h1>Blog Posts</h1>
       <div>
         {posts.map((post) => (
-          <a href={`/blog/${post.id}`} key={post.id}>
-            <span>{post.postTitle}</span>
-            <p> {post.postText}</p>
-          </a>
+          <div key={post.id}>
+            {post.published === true && (
+              <a href={`/blog/post/${post.id}`}>
+                <span>{post.postTitle}</span>
+                <p>{post.postText}</p>
+              </a>
+            )}
+          </div>
         ))}
       </div>
     </div>
