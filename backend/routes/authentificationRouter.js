05f4f80401df5d6df14f408ca9c1fb708325a178
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-11-14 18:27:42 +0100

    Remove check session route

--- backend/routes/authentificationRouter.js
+++ backend/routes/authentificationRouter.js
@@ -6,6 +6,5 @@ const router = express.Router();
 router.post("/user", authentificationController.registerUser);
 router.post("/login", authentificationController.loginUser);
 router.post("/logout", authentificationController.logoutUser);
-router.get("/check/session", authentificationController.checkSession);
 
 module.exports = router;
