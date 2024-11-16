632923477ea79969187fd834c5c5c4e151f5def1
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-11-16 21:28:39 +0100

    Add correct semantic elements, classnames

--- frontend-reader/src/components/Navbar/Navbar.jsx
+++ frontend-reader/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { getUserInfo } from "../../utils/getUserInfo";
 import { UserTokenContext } from "../../app/App";
+import navbar from "./Navbar.module.css";
 
 const Navbar = () => {
   const [user, setUser] = useState(null);
@@ -28,33 +29,52 @@ const Navbar = () => {
   };
 
   return (
-    <div>
-      {user ? (
-        <div>
-          <Link to={"/blog"}>Go to Blog</Link>
-          <p></p>
-          <Link to={"/register"}>Register</Link>
-          <p></p>
-          {user.userRole == "author" && (
-            <Link to={"/post"}>View all Posts</Link>
-          )}
-          <p></p>
-          <p>Id: {user.userId}</p>
-          <h2>Welcome, {user.username}!</h2>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <div>
-          <Link to={"/blog"}>Go to Blog</Link>
-          <p></p>
-          <Link to={"/register"}>Register</Link>
-          <p></p>
-          <Link to={"/login"}>Login</Link>
-          <p></p>
-        </div>
-      )}
+    <nav className={navbar.navbar}>
+      <Link className={navbar.link} to={"/blog"}>
+        Blog
+      </Link>
+      <section className={navbar.navSection}>
+        {user ? (
+          <>
+            <h2 className={navbar.navbarHeader}>Welcome, {user.username}!</h2>
+            {user.userRole == "author" && (
+              <>
+                <Link className={navbar.navLink} to={"/post"}>
+                  Edit-Posts
+                </Link>
+                <Link to="/post" className={`${navbar.navIcon} material-icons`}>
+                  edit
+                </Link>
+              </>
+            )}
+            <Link
+              to={"/blog"}
+              className={navbar.navLink}
+              onClick={handleLogout}
+            >
+              Logout
+            </Link>
+            <Link
+              to={"/blog"}
+              className={`${navbar.navIcon} material-icons`}
+              onClick={handleLogout}
+            >
+              logout
+            </Link>{" "}
+          </>
+        ) : (
+          <>
+            <Link className={navbar.navLink} to={"/register"}>
+              Register
+            </Link>
+            <Link className={navbar.navLink} to={"/login"}>
+              Login
+            </Link>
+          </>
+        )}
+      </section>
       <Outlet />
-    </div>
+    </nav>
   );
 };
 
