d27a839882f6520bdbf96e4824ec79ee10ee1482
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-11-19 16:29:05 +0100

    Add Input component jsx, css files

---
+++ frontend-reader/src/components/Input/Input.jsx
@@ -0,0 +1,40 @@
+import PropTypes from "prop-types";
+import input from "./Input.module.css";
+
+const Login = ({
+  inputIdName,
+  inputType,
+  placeholderText,
+  inputValue,
+  onChangeFunction,
+  labelText,
+}) => {
+  return (
+    <>
+      <label htmlFor={inputIdName} className={input.label}>
+        <strong>{labelText}:</strong>
+      </label>
+      <input
+        id={inputIdName}
+        name={inputIdName}
+        type={inputType}
+        value={inputValue}
+        onChange={onChangeFunction}
+        placeholder={placeholderText}
+        className={input.input}
+      />
+    </>
+  );
+};
+
+Login.propTypes = {
+  inputIdName: PropTypes.string.isRequired,
+  inputType: PropTypes.string.isRequired,
+  placeholderText: PropTypes.string.isRequired,
+  inputValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+    .isRequired,
+  onChangeFunction: PropTypes.func.isRequired,
+  labelText: PropTypes.string.isRequired,
+};
+
+export default Login;
