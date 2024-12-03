f019149ba31be574958c30d77588dea945176c84
Mustafa Ozkara <mustafaozkara@outlook.fr>
2024-12-03 16:58:00 +0100

    Add error element

--- frontend-reader/src/app/router.jsx
+++ frontend-reader/src/app/router.jsx
@@ -4,10 +4,11 @@ import Register from "../pages/Register/Register.jsx";
 import Login from "../pages/Login/Login.jsx";
 import Post from "../pages/Post/Post.jsx";
 import Posts from "../pages/Posts/Posts.jsx";
+import ErrorPage from "../pages/ErrorPage/ErrorPage.jsx";
 import { Route } from "react-router-dom";
 
 const routes = (
-  <Route path="/" element={<Navbar />}>
+  <Route path="/" element={<Navbar />} errorElement={<ErrorPage />}>
     <Route path="blog" element={<Blog />} />
     <Route path="blog/post/:postId" element={<Post />} />
     <Route path="post" element={<Posts />}></Route>
